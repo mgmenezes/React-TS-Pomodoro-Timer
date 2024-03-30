69c4ae438a3a11e5f5a47bb6c40808959ca86dce
Moises Menezes <moisesmenezes53@gmail.com>
2024-03-30 20:22:20 -0300

    feat: Adjusting inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,12 +15,26 @@ export const Home = () => {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput placeholder="Dê um nome para o seu projeto" type="text" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            type="text"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto1"></option>
+            <option value="Projeto2"></option>
+            <option value="Projeto3"></option>
+            <option value="aqui"></option>
+          </datalist>
           <label htmlFor="">Durante</label>
           <MinutesAmountInput
             placeholder="00"
             type="number"
             id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos</span>
         </FormContainer>
